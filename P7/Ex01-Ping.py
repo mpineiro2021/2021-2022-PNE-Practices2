a9a992db1eb930dbf79a3c6e1a3b0209fa9e49a7
marta <m.pineiro.2021@alumnos.urjc.es>
2022-05-23 21:09:21 +0200

    change

---
+++ P7/Ex01-Ping.py
@@ -0,0 +1,62 @@
+import http.client
+import json
+import termcolor
+
+PORT = 8080
+SERVER = "rest.ensembl.org"
+ENDPOINT = "/info/ping"
+PARAMS = "?content-type=application/json"
+URL = SERVER + ENDPOINT + PARAMS
+
+print()
+print(f"Server: {SERVER}")
+print(f"Url: {URL}")
+
+conn = http.client.HTTPConnection(SERVER, PORT)
+
+try:
+    conn.request("GET", URL)#recurso es \listusers
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+r1 = conn.getresponse()
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+if r1.status == HTTPStatus.OK:
+    print(f"Response received!: {r1.status} {r1.reason}\n")
+    print()
+
+
+data1 = r1.read().decode("utf-8")
+
+# -- Create a variable with the data,
+# -- form the JSON received
+person = json.loads(data1)
+
+print("CONTENT: ")
+
+# Print the information in the object
+print()
+termcolor.cprint("Name: ", 'green', end="")
+print(person['Firstname'], person['Lastname'])
+
+termcolor.cprint("Age: ", 'green', end="")
+print(person['age'])
+
+# Get the phoneNumber list
+phoneNumbers = person['phoneNumber']
+
+# Print the number of elements int the list
+termcolor.cprint("Phone numbers: ", 'green', end='')
+print(len(phoneNumbers))
+
+# Print all the numbers
+for i, num in enumerate(phoneNumbers):
+    termcolor.cprint("  Phone {}:".format(i), 'blue')
+
+    # The element num contains 2 fields: number and type
+    termcolor.cprint("    Type: ", 'red', end='')
+    print(num['type'])
+    termcolor.cprint("    Number: ", 'red', end='')
+    print(num['number'])
